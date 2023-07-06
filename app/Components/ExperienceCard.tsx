b6847dbf930e3f45a71d9afd51af247c4a5f0bb7
farooqmajeed <farooq306@hotmail.com>
2023-07-06 20:28:48 +0500

    added first expereince card image

--- app/Components/ExperienceCard.tsx
+++ app/Components/ExperienceCard.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion'
+
 
 type Props = {}
 
-function ExperienceCard({}: Props) {
+function ExperienceCard({ }: Props) {
   return (
-    <div>ExperienceCard</div>
+    <article className='flex'>
+      <motion.img
+        initial={{
+          y: -100,
+          opacity: 0
+        }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
+        src="/profile.jpeg"
+      />
+      <div>
+        
+      </div>
+    </article>
   )
 }
 
