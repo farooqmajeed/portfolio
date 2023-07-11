71085be45b9eeeea92d8873948daaa973695ef84
farooqmajeed <farooq306@hotmail.com>
2023-07-11 22:27:30 +0500

    added skills icons

--- app/Components/skills.tsx
+++ app/Components/skills.tsx
@@ -1,18 +1,48 @@
 "use client"
 
 import { motion } from 'framer-motion';
-import React from 'react'
+import React from 'react';
+import Skill from './Skill';
 
-function Skills() {
-  return (
-    <motion.div className='flex relative flex-col text-center md:text-left xl:flex-row
-    max-w-[200px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
- <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+type Props = {}
+
+function Skills({ }: Props) {
+    return (
+        <motion.div
+        initial={{ opacity: 0}}
+        whileInView={{ opacity:1}}
+        transition={{ duration: 2}}
+        className='flex relative flex-col text-center md:text-left xl:flex-row
+    max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
+            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
                 Skills
             </h3>
-
-    </motion.div>
-  )
+            <h3 className='absolute top-36  uppercase tracking-[3px] text-gray-500 text-sm'>
+                Hover over a skill for currency profiencency
+            </h3>
+            <div className='grid grid-cols-4 gap-5'>
+                <Skill directionLeft={true}
+                 skillImage={'https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg'} 
+                 experience='95%'/>
+                 <Skill directionLeft={true}
+                 skillImage={'https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg'} 
+                 experience='95%'/>
+                 <Skill directionLeft={true}
+                 skillImage={'https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg'} 
+                 experience='95%'/>
+                 <Skill directionLeft={true}
+                 skillImage={'https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg'} 
+                 experience='95%'/>
+                 <Skill directionLeft={true}
+                 skillImage={'https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg'} 
+                 experience='95%'/>
+                 <Skill directionLeft={true}
+                 skillImage={'https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg'} 
+                 experience='95%'/>
+                <Skill />
+            </div>
+        </motion.div>
+    )
 }
 
 export default Skills;
